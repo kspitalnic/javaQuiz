e7e614f2953a8c2367c56432979926401f256acc
Kate Spitalnic <kespitalnic@gmail.com>
2021-12-10 10:49:03 -0500

    html and css

---
+++ script.js
@@ -0,0 +1,37 @@
+//Intro
+
+var startBtn = document.querySelector(".intro button");
+
+startBtn.addEventListener("click", function(){
+    alert("button clicked");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+//High Score
+
+//var EL = document.createElement("li");
+//taskItemEl=textContent=user input;
+//taskitemEl
+//highscores.appendChild(taskitemEl);
+//taskItemEl.classname="task-item"
+//AKA e.g.,
+// var buttonEl = document.querySelector("#save-task");
+// var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+// var createTaskHandler = function() {
+//     var listItemEl = document.createElement("li");
+//     listItemEl.className = "task-item";
+//     listItemEl.textContent = "This is a new task.";
+//     tasksToDoEl.appendChild(listItemEl);
+// //   }
+// buttonEl.addEventListener("click", createtaskHandler);
