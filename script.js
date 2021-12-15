b4a038ffbbe91cd3388dea6577b79916044da011
Kate Spitalnic <kespitalnic@gmail.com>
2021-12-14 21:08:05 -0500

    gives feedback, runs through all questions but only shows questions 1,2, and 4 to user?

--- script.js
+++ script.js
@@ -5,98 +5,91 @@
 
 var startBtn = document.querySelector("#start");
 var questionEl = document.querySelector("#prompt");
-var currentQ = 0;
 var nextBtn = document.querySelector("#next");
-var currentOp = 0;
-var answerLetter = 0;
 var opa = document.querySelector("#a");
 var opb = document.querySelector("#b");
 var opc = document.querySelector("#c");
 var opd = document.querySelector("#d");
-
+var currentQ = -1;
+var currentOp = 0;
+var answerLetter = 0;
 
 var questions = [
-
     {
         que: "This is the first question that I am asking",
         options: [
             { a:"this is an answer choice"},
             { b: "this is an answer choice too" },
-            { c: "y" },
-            { d: "y" }
+            { c: "1c" },
+            { d: "1d" }
         ],
         answers: [
-            true,
-            false,
-            false,
-            false,
+            {a: false},
+            {b: false},
+            {c: true},
+            {d: false},
         ]
     },
-
     {
         que: "This is the second question I am asking",
         options: [
-            { a:"this is an answer choice"},
-            { b: "this is an answer choice too" },
-            { c: "y" },
-            { d: "y" }
+            { a:"2a"},
+            { b: "2b" },
+            { c: "2c" },
+            { d: "2d" }
         ],
         answers: [
-            true,
-            false,
-            false,
-            false,
+            {a: false},
+            {b: false},
+            {c: true},
+            {d: false},
         ]
     },
-
     {
         que: "This is the third question that I am asking",
         options: [
-            { a:"this is an answer choice"},
-            { b: "this is an answer choice too" },
-            { c: "y" },
-            { d: "y" }
+            { a:"3a"},
+            { b: "3b" },
+            { c: "3c" },
+            { d: "3d" }
         ],
         answers: [
-            true,
-            false,
-            false,
-            false,
+            {a: false},
+            {b: false},
+            {c: true},
+            {d: false},
         ]
     },
-
     {
         que: "This is the fourth question I am asking",
         options: [
-            { a:"this is an answer choice"},
-            { b: "this is an answer choice too" },
-            { c: "y" },
-            { d: "y" }
+            { a:"4a"},
+            { b: "4b" },
+            { c: "4c" },
+            { d: "4d" }
         ],
         answers: [
-            true,
-            false,
-            false,
-            false,
+            {a: false},
+            {b: false},
+            {c: true},
+            {d: false},
         ]
     },
-
     {
         que: "fifth question",
         options: [
-            { a:"this is an answer choice"},
-            { b: "this is an answer choice too" },
-            { c: "y" },
-            { d: "y" }
+            { a:"5a"},
+            { b: "5b" },
+            { c: "5c" },
+            { d: "5d" }
         ],
         answers: [
-            true,
-            false,
-            false,
-            false,
+            {a: false},
+            {b: false},
+            {c: true},
+            {d: false},
         ]
     },
-
 ]
 
 
@@ -114,8 +107,13 @@ function start() {
 start();
 
 // function for question 
+//ONLY ITERATES THROUGH IT CORRECTLY FOR THE FIRST TIME (  which is why originally had the two separete functions - the second for future questions... find out why and debug. then just need timer and to add to timer if wrong).
 
 function askFirstQ() {
+    currentQ++;
+    currentOp=0;
+
+    if (currentQ < 5) {
     console.log(questionEl.textContent = questions[currentQ].que);
 
     console.log(opa.textContent = questions[currentQ].options[currentOp].a);
@@ -125,85 +123,77 @@ function askFirstQ() {
     console.log(opc.textContent = questions[currentQ].options[currentOp].c);
     currentOp++;
     console.log(opd.textContent = questions[currentQ].options[currentOp].d);
+    currentOp=0;
+
+    nextBtn.textContent="Review";
 
     nextBtn.addEventListener("click", function () {
         nextBtn.textContent="Next";
+
             var respa = document.getElementById("aa").checked;
             var respb = document.getElementById("bb").checked;
             var respc = document.getElementById("cc").checked;
             var respd = document.getElementById("dd").checked;
-            console.log(respa);
-            console.log(respb);
-            console.log(questions[currentQ].answers[0]);
-            if ((respa) && (questions[currentQ].answers[0])) {
-            console.log(questionEl.textContent = "Correct!");
-            }
 
-            else if ((respb) && (questions[currentQ].answers[1])) {
+            if (((respa) && (questions[currentQ].answers[0].a))||
+                ((respb) && (questions[currentQ].answers[1].b))||
+                ((respc) && (questions[currentQ].answers[2].c))||
+                ((respd) && (questions[currentQ].answers[3].d))) {
                 console.log(questionEl.textContent = "Correct!");
-            }
-            else if ((respc) && (questions[currentQ].answers[2])) {
-                console.log(questionEl.textContent = "Correct!");
-            }
-            else if ((respd) && (questions[currentQ].answers[3])) {
-                console.log(questionEl.textContent = "Correct!");
+                nextBtn.addEventListener("click",function(){
+                    askFirstQ();
+            })}
+                       else if((!respa)&&(!respb)&&(!respc)&&(!respd)){
+                window.alert("Please select an option");
+                nextBtn.textContent="Review";
+                askFirstQ();
             }
             else {
                 console.log(questionEl.textContent = "Incorrect");
+                nextBtn.addEventListener("click",function(){
+                    askFirstQ();
+            })
             }
-        
-            currentQ++;
-
-        askQuestion();
-    })
-
-    // nextBtn.addEventListener("click", feedback())
-    // function feedback(){
-    //     var respa = document.getElementsByName("a")
-    //     var respb = document.getElementsByName("b")
-    //     var respc = document.getElementsByName("c")
-    //     var respd = document.getElementsByName("d")
-    //     if ((respa=true)  && (questions[currentQ].options.cora =true)) {
-    //         console.log(questionEl.textContent = "Correct!");
-    //     }
-    //     else if ((respb=true)  && (questions[currentQ].options.corb =true)) {
-    //             console.log(questionEl.textContent = "Correct!");
-    //     }
-    //     else if ((respc=true)  && (questions[currentQ].options.corc =true)) {
-    //             console.log(questionEl.textContent = "Correct!");
-    //     }
-    //     else if ((respd=true)  && (questions[currentQ].options.cord =true)) {
-    //             console.log(questionEl.textContent = "Correct!");
-    //     }
-    //     else {
-    //         console.log(questionEl.textContent = "Incorrect");
-    //     }}
-}
-
-function askQuestion() {
-    nextBtn.addEventListener("click", function () {
-        document.querySelector('input[name="select"]:checked').checked = false;
-        if (currentQ < 5) {
-            console.log(questionEl.textContent = questions[currentQ].que);
-            currentOp = 0;
-            console.log(opa.textContent = questions[currentQ].options[currentOp].a);
-            currentOp++;
-            console.log(opb.textContent = questions[currentQ].options[currentOp].b);
-            currentOp++;
-            console.log(opc.textContent = questions[currentQ].options[currentOp].c);
-            currentOp++;
-            console.log(opd.textContent = questions[currentQ].options[currentOp].d);
-            currentOp++;
-            currentQ++;
-        }
-        // else { 
-        // document.getElementsByName("next").onclick = function(){
-        //     location.href = ""
-        // }
-        //next function
+        })
     }
-    )
+    else {
+        console.log("done");
+        // window.location.href = "./highscores.html"
+    }
 }
+ 
+// function nextQ(){
+// nextBtn.addEventListener("click",function(){
+//     currentQ++;
+//     // document.querySelector('input[name="select"]:checked').checked = false;
+//     askFirstQ();
+// })}
+
+// function askQuestion() {
+//     nextBtn.addEventListener("click", function () {
+//         nextBtn.textContent="Review"
+//         document.querySelector('input[name="select"]:checked').checked = false;
+//         if (currentQ < 5) {
+//             console.log(questionEl.textContent = questions[currentQ].que);
+//             currentOp = 0;
+//             console.log(opa.textContent = questions[currentQ].options[currentOp].a);
+//             currentOp++;
+//             console.log(opb.textContent = questions[currentQ].options[currentOp].b);
+//             currentOp++;
+//             console.log(opc.textContent = questions[currentQ].options[currentOp].c);
+//             currentOp++;
+//             console.log(opd.textContent = questions[currentQ].options[currentOp].d);
+//             currentOp++;
+//             currentQ++;
+//         }
+//         // else { 
+//         // document.getElementsByName("next").onclick = function(){
+//         //     location.href = ""
+//         // }
+//         //next function
+//     }
+//     )
+// }
 
 
 // console.log(questions[i]);
@@ -236,4 +226,30 @@ function askQuestion() {
 //     //Female radio button is checked
 //   }
 
+
+
+
+
 //if (document.getElementById(a))
+
+    //         }
+    //         else if ((respb) && (questions[currentQ].answers[1].b)) {
+    //             console.log(questionEl.textContent = "Correct!");
+    //             nextBtn.addEventListener("click",function(){
+    //                 askFirstQ();
+    //         })
+    //         }
+    //         else if ((respc) && (questions[currentQ].answers[2].c)) {
+    //             console.log(questionEl.textContent = "Correct!");
+    //             nextBtn.addEventListener("click",function(){
+    //                 askFirstQ();
+    //         })
+    //         }
+    //         else if ((respd) && (questions[currentQ].answers[3].d)) {
+    //             console.log(questionEl.textContent = "Correct!");
+    //             nextBtn.addEventListener("click",function(){
+    //                 askFirstQ();
+    //         })
+    //         }
+ 
+    // })
