a3201c97db1514acb94701f93d6ad3ccb8797a08
Kate Spitalnic <kespitalnic@gmail.com>
2021-12-19 01:24:07 -0500

    questions added

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-//need actual questions
-//Issues: 
-    // Iteration through questions visible to user. 
-    // pop up comes three times when finished complete. 
-    // time goes down extra when don't put an answer (seems to add up the non-answers as incorrect?)
-
-    //extra - pause time when reviewing answer?
-
 var startBtn = document.querySelector("#start");
 var questionEl = document.querySelector("#prompt");
 var nextBtn = document.querySelector("#next");
@@ -18,97 +10,99 @@ var currentQ = 0;
 var currentOp = 0;
 var answerLetter = 0;
 
-var time=100;
+var time = 100;
 
 
-document.getElementById("start").addEventListener("click", function(){
+document.getElementById("start").addEventListener("click", function () {
 
-var interval=setInterval(function timer(){
-    document.getElementById("clock").innerHTML=time;
-    time--;
-if (time<=-1){
-    clearInterval(interval);
-    nextBtn.disabled=true;
-    document.getElementById("clock").innerHTML="0";
-    window.alert("You ran out of time, Try again!");       
-    window.location.href = "#"}}, 1000);
+    var interval = setInterval(function timer() {
+        document.getElementById("clock").innerHTML = time;
+        time--;
+        if (time <= -1) {
+            clearInterval(interval);
+            nextBtn.disabled = true;
+            document.getElementById("clock").innerHTML = "0";
+            window.alert("You ran out of time, Try again!");
+            window.location.href = "#"
+        }
+    }, 1000);
     console.log(time);
 })
 
 var questions = [
     {
-        que: "This is the first question that I am asking",
+        que: "Which method outputs a message to the web console?",
         options: [
-            { a:"this is an answer choice"},
-            { b: "this is an answer choice too" },
-            { c: "1c" },
-            { d: "1d" }
+            { a: "console.log()" },
+            { b: "window.alert('message here')" },
+            { c: "//" },
+            { d: "window.prompt('message here')" }
         ],
         answers: [
-            {a: false},
-            {b: false},
-            {c: true},
-            {d: false},
+            { a: true },
+            { b: false },
+            { c: false },
+            { d: false },
         ]
     },
     {
-        que: "This is the second question I am asking",
+        que: "What are constructs that are built into the browser for efficent JavaScript coding?",
         options: [
-            { a:"2a"},
-            { b: "2b" },
-            { c: "2c" },
-            { d: "2d" }
+            { a: "JSON" },
+            { b: "function" },
+            { c: "APIs" },
+            { d: "Hashtags" }
         ],
         answers: [
-            {a: false},
-            {b: false},
-            {c: true},
-            {d: false},
+            { a: false },
+            { b: false },
+            { c: true },
+            { d: false },
         ]
     },
     {
-        que: "This is the third question that I am asking",
+        que: "What kind of data can you store in local storage?",
         options: [
-            { a:"3a"},
-            { b: "3b" },
-            { c: "3c" },
-            { d: "3d" }
+            { a: "objects" },
+            { b: "strings" },
+            { c: "integers" },
+            { d: "binary" }
         ],
         answers: [
-            {a: false},
-            {b: false},
-            {c: true},
-            {d: false},
+            { a: false },
+            { b: true },
+            { c: false },
+            { d: false },
         ]
     },
     {
-        que: "This is the fourth question I am asking",
+        que: "What tag do you use to link a JavaScript file to HTML",
         options: [
-            { a:"4a"},
-            { b: "4b" },
-            { c: "4c" },
-            { d: "4d" }
+            { a: "<link>" },
+            { b: "<main>" },
+            { c: "<script>" },
+            { d: "<head>" }
         ],
         answers: [
-            {a: false},
-            {b: false},
-            {c: true},
-            {d: false},
+            { a: false },
+            { b: false },
+            { c: true },
+            { d: false },
         ]
     },
     {
-        que: "fifth question",
+        que: "What method returns the first element in the document that matches its selector?",
         options: [
-            { a: "5a"},
-            { b: "5b" },
-            { c: "5c" },
-            { d: "5d" }
+            { a: "document.addEverntListener()" },
+            { b: "document.setItem()" },
+            { c: "document.getElementById()" },
+            { d: "document.querySelector()" }
         ],
         answers: [
-            {a: false},
-            {b: false},
-            {c: true},
-            {d: false},
+            { a: false },
+            { b: false },
+            { c: false },
+            { d: true },
         ]
     },
 ]
@@ -128,123 +122,117 @@ function start() {
 start();
 
 // function for question 
-//iterates through all of them but only shows some to user. error at end. 
 
 function askFirstQ() {
-    // currentQ++;
-    currentOp=0;
+    currentOp = 0;
 
     document.getElementById("aa").disabled = false;
     document.getElementById("bb").disabled = false;
     document.getElementById("cc").disabled = false;
-    document.getElementById("dd").disabled = false; 
+    document.getElementById("dd").disabled = false;
 
     if (currentQ < 5) {
-    console.log(questionEl.textContent = questions[currentQ].que);
+        console.log(questionEl.textContent = questions[currentQ].que);
 
-    console.log(opa.textContent = questions[currentQ].options[currentOp].a);
-    currentOp++;
-    console.log(opb.textContent = questions[currentQ].options[currentOp].b);
-    currentOp++;
-    console.log(opc.textContent = questions[currentQ].options[currentOp].c);
-    currentOp++;
-    console.log(opd.textContent = questions[currentQ].options[currentOp].d);
-    currentOp=0;
+        console.log(opa.textContent = questions[currentQ].options[currentOp].a);
+        currentOp++;
+        console.log(opb.textContent = questions[currentQ].options[currentOp].b);
+        currentOp++;
+        console.log(opc.textContent = questions[currentQ].options[currentOp].c);
+        currentOp++;
+        console.log(opd.textContent = questions[currentQ].options[currentOp].d);
+        currentOp = 0;
 
-    // rvwBtn.textContent="Review"; 
-    rvwBtn.classList.remove("hide");
-    nextBtn.setAttribute("class", "hide");
+        rvwBtn.classList.remove("hide");
+        nextBtn.setAttribute("class", "hide");
 
-    rvwBtn.addEventListener("click", reviewQ);
+        rvwBtn.addEventListener("click", reviewQ);
 
     }
-    else{
+    else {
 
-        if (time<=0){
+        if (time <= 0) {
             window.alert("Please try again!");
         }
-        else{
-        console.log("done " + time);
-        time=time;
-        score=JSON.stringify(time);
-        console.log(score);
-        clearInterval();
-        var input =window.prompt("Congragulations! You scored " + time +  " points on the quiz! Add your initials to the high score board!" );    
+        else {
+            console.log("done " + time);
+            time = time;
+            score = JSON.stringify(time);
+            console.log(score);
+            clearInterval();
+            var input = window.prompt("Congragulations! You scored " + time + " points on the quiz! Add your initials to the high score board!");
 
-        window.location.href = "./highscores.html"   
+            window.location.href = "./highscores.html"
 
-        //save to local storage 
+            //save to local storage 
 
-        var hs = JSON.parse(localStorage.getItem("hs")) || [];
-        var scoreObject= {
-            "score":score,
-            "input":input,
+            var hs = JSON.parse(localStorage.getItem("hs")) || [];
+            var scoreObject = {
+                "score": score,
+                "input": input,
+            }
+            hs.push(scoreObject);
+            hs.sort((a, b) => b.score - a.score);
+
+            localStorage.setItem("hs", JSON.stringify(hs));
         }
-        hs.push(scoreObject);
-        hs.sort((a,b)=> b.score-a.score);
-
-        localStorage.setItem("hs", JSON.stringify(hs));
-    }}
+    }
 }
 
-    function reviewQ () {
+function reviewQ() {
 
-        rvwBtn.setAttribute("class", "hide");
-        nextBtn.classList.remove("hide");
+    rvwBtn.setAttribute("class", "hide");
+    nextBtn.classList.remove("hide");
 
-    // nextBtn.addEventListener("click", function () {
-        // nextBtn.textContent="Next";
+    document.getElementById("aa").disabled = true;
+    document.getElementById("bb").disabled = true;
+    document.getElementById("cc").disabled = true;
+    document.getElementById("dd").disabled = true;
 
-        document.getElementById("aa").disabled = true;
-        document.getElementById("bb").disabled = true;
-        document.getElementById("cc").disabled = true;
-        document.getElementById("dd").disabled = true;
+    var respa = document.getElementById("aa").checked;
+    var respb = document.getElementById("bb").checked;
+    var respc = document.getElementById("cc").checked;
+    var respd = document.getElementById("dd").checked;
 
-            var respa = document.getElementById("aa").checked;
-            var respb = document.getElementById("bb").checked;
-            var respc = document.getElementById("cc").checked;
-            var respd = document.getElementById("dd").checked;
+    if (((respa) && (questions[currentQ].answers[0].a)) ||
+        ((respb) && (questions[currentQ].answers[1].b)) ||
+        ((respc) && (questions[currentQ].answers[2].c)) ||
+        ((respd) && (questions[currentQ].answers[3].d))) {
+        console.log(questionEl.textContent = "Correct!");
+        nextBtn.addEventListener("click", function () {
+            //get an error message for following line. not yet resolved
+            if (document.querySelector('input[name="select"]:checked').checked = true) {
+                document.querySelector('input[name="select"]:checked').checked = false
+            };
+            currentQ++;
 
-            if (((respa) && (questions[currentQ].answers[0].a))||
-                ((respb) && (questions[currentQ].answers[1].b))||
-                ((respc) && (questions[currentQ].answers[2].c))||
-                ((respd) && (questions[currentQ].answers[3].d))) {
-                console.log(questionEl.textContent = "Correct!");
-                nextBtn.addEventListener("click",function(){
-                    // if (document.querySelector('input[name="select"]:checked').checked = true){
-                        document.querySelector('input[name="select"]:checked').checked = false
-                    // };
-                    currentQ++;
+            askFirstQ();
+        })
+    }
 
-                askFirstQ();
-            })}
+    else if (((respa) && (!questions[currentQ].answers[0].a)) ||
+        ((respb) && (!questions[currentQ].answers[1].b)) ||
+        ((respc) && (!questions[currentQ].answers[2].c)) ||
+        ((respd) && (!questions[currentQ].answers[3].d))) {
+        console.log(questionEl.textContent = "Incorrect");
+        if (time > -1) { time = time - 10 }
+        else { time = 0 };
+        nextBtn.addEventListener("click", function () {
+            //get an error message for following line. not resolved
+            if (document.querySelector('input[name="select"]:checked').checked = true) {
+                document.querySelector('input[name="select"]:checked').checked = false;
+            };
+            currentQ++;
+            askFirstQ();
+        })
+    }
+    
+    else {
+        window.alert("Please select an option");
 
-            else if (((respa) && (!questions[currentQ].answers[0].a))||
-                     ((respb) && (!questions[currentQ].answers[1].b))||
-                     ((respc) && (!questions[currentQ].answers[2].c))||
-                     ((respd) && (!questions[currentQ].answers[3].d))){
-                console.log(questionEl.textContent = "Incorrect");
-                if (time>-1)
-                    {time=time-10}
-                else{time=0};
-                nextBtn.addEventListener("click",function(){
-                    if (document.querySelector('input[name="select"]:checked').checked = true){
-                    document.querySelector('input[name="select"]:checked').checked = false;
-                };
-                    currentQ++;
-                    askFirstQ();
-            
-            })}
-
-            else{
-                window.alert("Please select an option");
-                // nextBtn.textContent="Review";
-                // currentQ--;
-                askFirstQ();
-            }
-
-    // })
-        }
+        askFirstQ();
+    }
+}
 
 
 
